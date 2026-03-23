9ef6b229efd11edeb979c5254f56d7dbb660e22d
Amy Wilder <henrythepony@gmail.com>
2026-03-23 15:24:53 -0400

    Fix bad import

--- src/win/wrapper_windows.rs
+++ src/win/wrapper_windows.rs
@@ -1,21 +1,24 @@
 //! Ensures the program can compile whether we're on windows or not, even if it can't operate outside of windows
 
-pub use windows::{Win32::{
-    Graphics::Gdi::{
-        BI_RGB, BITMAPINFO, BITMAPINFOHEADER, BitBlt, CreateCompatibleBitmap, CreateCompatibleDC,
-        DIB_RGB_COLORS, DeleteDC, DeleteObject, GetDC, GetDIBits, HBITMAP, HDC, HGDIOBJ, RGBQUAD,
-        ReleaseDC, SRCCOPY, SelectObject,
-    },
-    UI::{
-        Input::KeyboardAndMouse::{
-            GetAsyncKeyState, INPUT, INPUT_KEYBOARD, MOUSEEVENTF_ABSOLUTE, MOUSEEVENTF_LEFTDOWN,
-            MOUSEEVENTF_LEFTUP, SendInput,
-            INPUT_0, KEYBD_EVENT_FLAGS, KEYBDINPUT, KEYEVENTF_KEYUP, VIRTUAL_KEY,
-            INPUT_MOUSE, MOUSE_EVENT_FLAGS, MOUSEEVENTF_MOVE, MOUSEINPUT,
+pub use windows::{
+    Win32::{
+        Foundation::POINT,
+        Graphics::Gdi::{
+            BI_RGB, BITMAPINFO, BITMAPINFOHEADER, BitBlt, CreateCompatibleBitmap,
+            CreateCompatibleDC, DIB_RGB_COLORS, DeleteDC, DeleteObject, GetDC, GetDIBits, HBITMAP,
+            HDC, HGDIOBJ, RGBQUAD, ReleaseDC, SRCCOPY, SelectObject,
         },
-        WindowsAndMessaging::{
-            GetCursorPos, GetSystemMetrics, SM_CXVIRTUALSCREEN, SM_CYVIRTUALSCREEN,
+        UI::{
+            Input::KeyboardAndMouse::{
+                GetAsyncKeyState, INPUT, INPUT_0, INPUT_KEYBOARD, INPUT_MOUSE, KEYBD_EVENT_FLAGS,
+                KEYBDINPUT, KEYEVENTF_KEYUP, MOUSE_EVENT_FLAGS, MOUSEEVENTF_ABSOLUTE,
+                MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_LEFTUP, MOUSEEVENTF_MOVE, MOUSEINPUT, SendInput,
+                VIRTUAL_KEY,
+            },
+            WindowsAndMessaging::{
+                GetCursorPos, GetSystemMetrics, SM_CXVIRTUALSCREEN, SM_CYVIRTUALSCREEN,
+            },
         },
     },
-    Win32::Foundation::POINT
-}, core::Result};
+    core::Result,
+};
